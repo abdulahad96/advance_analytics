155693fbb411e0568a1be27198468736e45a70b3
AbdulahadItsol <abdul.ahad@itsolution24x7.com>
2023-06-02 22:27:07 +0500

    prisma setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TanentModule } from './tanent/tanent.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TanentModule],
   controllers: [AppController],
   providers: [AppService],
 })
